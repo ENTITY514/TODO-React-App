4cb5ecc932ddd0f249a6fc6fc1fed6ab978f992e
ENTITY514 <entity514@mail.ru>
2022-07-07 14:07:02 +0600

    UI changes

--- src/Pages/Main_Page/Components/Todo_List/Components/Todo-item/todo-item.jsx
+++ src/Pages/Main_Page/Components/Todo_List/Components/Todo-item/todo-item.jsx
@@ -6,8 +6,7 @@ const TODO_Item = ({ item, removeTask }) => {
   return (
       <div key={item.id} className="todo_container">
         <div className={style.text}>{item.work_text}</div>
-        <div className={style.done} onClick={() => { removeTask(item.id) }}><Icon icon_src="icons/check.png" /></div>
-        <div className={style.delete} onClick={() => { removeTask(item.id) }}><Icon icon_src="icons/remove.png" /></div>
+        <div className={style.done} onClick={() => { removeTask(item.id) }}><Icon icon_src="icons/cross-mark.png" /></div>
       </div>
   )
 }
